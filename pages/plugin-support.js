3e9c17f859a9fe70fe7f3bf41f518fe7dc587a0b
Ben Rogerson <info@benrogerson.com.au>
2020-07-22 07:48:52 +0930

    add timsort

--- pages/plugin-support.js
+++ pages/plugin-support.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { pluginSupportData } from './../data'
 import LayoutDefault from './../layouts/default'
 import { SupportCard } from './../components'
+import { sort } from 'timsort'
 
 const sortBy = [
   {
@@ -14,25 +15,28 @@ const sortBy = [
   },
 ]
 
+const multiSort = (a, b) => {
+  let i = 0,
+    result = 0
+  while (i < sortBy.length && result === 0) {
+    result =
+      sortBy[i].direction *
+      (a[sortBy[i].prop] || -1 < b[sortBy[i].prop] || -1
+        ? -1
+        : a[sortBy[i].prop] || -1 > b[sortBy[i].prop] || -1
+        ? 1
+        : 0)
+    i++
+  }
+  return result
+}
+
 const Page = () => {
-  const sortedData = pluginSupportData.sort((a, b) => {
-    let i = 0,
-      result = 0
-    while (i < sortBy.length && result === 0) {
-      result =
-        sortBy[i].direction *
-        (a[sortBy[i].prop] || -1 < b[sortBy[i].prop] || -1
-          ? -1
-          : a[sortBy[i].prop] || -1 > b[sortBy[i].prop] || -1
-          ? 1
-          : 0)
-      i++
-    }
-    return result
-  })
+  // TODO: Cache this data and sort once
+  sort(pluginSupportData, multiSort)
   return (
     <LayoutDefault pageTitle="Tailwind plugin support">
-      {sortedData.map(SupportCard)}
+      {pluginSupportData.map(SupportCard)}
     </LayoutDefault>
   )
 }
